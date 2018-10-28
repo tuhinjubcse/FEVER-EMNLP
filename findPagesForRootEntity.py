e414d4f5be34b21b4f845886cf1efc4d94b2dcac
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-10-28 14:11:49 -0400

    Create findPagesForRootEntity.py

---
+++ findPagesForRootEntity.py
@@ -0,0 +1,28 @@
+import json
+import nltk
+import wikipedia
+import ast
+import json
+import unicodedata
+
+f = open('root_entity_test.txt','w')
+
+
+count = 1
+for line in open('shared_task_test.jsonl','r'):
+	line = json.loads(line.strip())
+	claim = nltk.word_tokenize(line['claim'])
+	pos = nltk.pos_tag(claim)
+	s = ''
+	for w,p in zip(claim,pos):
+		if p[1][0]=='V' and w.islower():
+			s = s[:-1]
+			break
+		s = s+ w + ' '
+	entity = wikipedia.search(s,1)
+	print(entity,count)
+	if len(entity)>0:
+		f.write(json.dumps([entity[0]])+'\n')
+	else:
+		f.write(json.dumps([])+'\n')
+	count = count+1
