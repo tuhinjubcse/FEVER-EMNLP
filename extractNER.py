e0c45203670e05c19c62c2c5d906bda72dbce653
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-10-28 14:23:30 -0400

    Update extractNER.py

--- extractNER.py
+++ extractNER.py
@@ -5,10 +5,10 @@ import json
 #and then calls pretrained model to get the Named entities and stores them in file in order 
 
 
-f = open('NER_test_txt','w')
+f = open('NER_dev_txt','w')
 from allennlp.service.predictors import Predictor
 predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/ner-model-2018.04.26.tar.gz")
-for line in open('blind_test.txt','r'):
+for line in open('NER_shared_dev','r'):
 	line = line.strip()
 	results = predictor.predict(sentence=line[0])
 	for word, tag in zip(results["words"], results["tags"]):
